1e515cd036c53f4efc17861e9a987f99e5735331
smoking_yoda <collinsdefrutos@gmail.com>
2026-06-10 15:27:26 -0400

    player added & main, constants update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 
@@ -10,11 +11,13 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
         screen.fill("black")
         dt = clock.tick(60) / 1000.0
+        player.draw(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
