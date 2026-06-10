d300e9d969f667a3632be2aa0aec34b2c6bb93d7
smoking_yoda <collinsdefrutos@gmail.com>
2026-06-10 14:56:42 -0400

    main.py update & circleshape.py add

--- main.py
+++ main.py
@@ -8,25 +8,24 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
 
     while True:
         log_state()
+        screen.fill("black")
+        dt = clock.tick(60) / 1000.0
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-    screen.fill("black")
-
-    pygame.display.flip()
-
-
-
-
+      
+        pygame.display.flip()
+        
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
-
+    
 if __name__ == "__main__":
     main()
