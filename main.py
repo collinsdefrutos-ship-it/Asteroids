9b9315bcde8396d459b9f93c08d63244b5b13003
smoking_yoda <collinsdefrutos@gmail.com>
2026-06-10 17:07:39 -0400

    shot.py added

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
+import sys
+
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from logger import log_event
+
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 
@@ -15,8 +20,10 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = updatable, drawable
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
     dt = 0.0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -27,6 +34,11 @@ def main():
         screen.fill("black")
         dt = clock.tick(60) / 1000.0
         updatable.update(dt)
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         for element in drawable:
             element.draw(screen)
 
--- shot.py
+++ shot.py
@@ -1,14 +1,14 @@
-from constants import *
-import pygame
 from circleshape import CircleShape
+from constants import SHOT_RADIUS, LINE_WIDTH
+import pygame
 
-class Asteroid(CircleShape):
+class Shot(CircleShape):
     def __init__(self, x: float, y: float, radius: float) -> None:
         super().__init__(x, y, radius)
         self.radius = radius
 
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
-    
+
     def update(self, dt: float) -> None:
         self.position += self.velocity * dt
